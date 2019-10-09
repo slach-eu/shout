b917b4e682e833f356b1e520b53bddba0362b96b
Sebastian Łach <root@slach.eu>
2019-10-09 11:11:35 +0200

    Refactor application script

--- app.py
+++ app.py
@@ -1,50 +1,25 @@
 #!/usr/bin/env python
-from random import randint
-from flask import jsonify
 from flask_restful import Api
-from models.tweet import Tweet
 from services import config
 from services import registry
 from models.database import db
-from models.user import User
 from resources.user import UserResource
 from resources.tweet import TweetResource
 
 
+# configure application
 service = registry.get("application_service", config)
 app = service.get_app()
+
+# configure api
 api = Api(app)
+api.add_resource(UserResource, '/api/user/<user_id>')
+api.add_resource(TweetResource, '/api/tweet/<tweet_id>')
 
 
-@app.route('/api/tweets', methods=['GET'])
-def get_tweets():
-    n = randint(100, 100*100)
-    user = User(
-        username='guest{}'.format(n),
-        email='guest{}@example.com'.format(n),
-    )
-    db.session.add(user)
-    tweet = Tweet(
-        content='Some shitty tweet',
-        tag='shit',
-        user=user,
-    )
-    db.session.add(tweet)
-    db.session.commit()
-    tweets = Tweet.query.all()
-    return jsonify({'tweets': [repr(t) for t in tweets]})
-
-
-def run_app():
+if __name__ == '__main__':
     db.init_app(app)
     with app.app_context():
         db.create_all()
         db.session.commit()
     app.run(debug=True)
-
-
-api.add_resource(UserResource, '/api/user/<user_id>')
-api.add_resource(TweetResource, '/api/tweet/<tweet_id>')
-
-if __name__ == '__main__':
-    run_app()
--- resources/tweet.py
+++ resources/tweet.py
@@ -1,5 +1,8 @@
+from random import randint
 from flask_restful import Resource
 from models.tweet import Tweet
+from models.user import User
+from models.database import db
 
 
 class TweetResource(Resource):
@@ -10,3 +13,20 @@ class TweetResource(Resource):
             'tag': '#{}'.format(tweet.tag),
             'tweet': '{}'.format(tweet.content),
         }
+
+    def post(self):
+        n = randint(100, 100 * 100)
+        user = User(
+            username='guest{}'.format(n),
+            email='guest{}@example.com'.format(n),
+        )
+        db.session.add(user)
+        tweet = Tweet(
+            content='Some shitty tweet',
+            tag='shit',
+            user=user,
+        )
+        db.session.add(tweet)
+        db.session.commit()
+        tweets = Tweet.query.all()
+        return {'tweets': [repr(t) for t in tweets]}
